9572bf958bbd01d244114c26db80c047ef945b03
maynak singh <153384908+m-k-singh1@users.noreply.github.com>
2024-01-20 11:21:45 +0000

    maths

--- 01_basics.js/test.js
+++ 01_basics.js/test.js
@@ -1,5 +1,36 @@
-console.log("mayank");
-console.log("kumar");
-console.log("singh");
-let firstOne = "my first code on j.s";
-console.log(firstOne);
+// console.log("mayank");
+// console.log("kumar");
+// console.log("singh");
+// let firstOne = "my first code on j.s";
+// console.log(firstOne);
+
+//example of string 
+
+let name = "mayank singh"
+//console.log(name);
+
+//example on numbers 
+
+let num = 200 
+//console.log(num);
+
+let num2 = new Number (200)
+console.log(num2);
+
+//another example 
+
+const number = 1000000000
+//console.log(number.toLocaleString('en-IN'));
+
+// ******************math's*************************
+
+//console.log(Math);
+//console.log(Math.abs('-5'));    //-ve to +ve
+//console.log(Math.round('5.6'));  // round of number
+
+// ***********example********************
+
+const min = 20 
+const max = 30 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
