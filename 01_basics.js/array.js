20ceef4529be6f0fd4f5104888b8b33f14569985
maynak singh <mayank23378@gmail.com>
2024-01-21 09:32:10 +0000

    first part of array

---
+++ 01_basics.js/array.js
@@ -0,0 +1,28 @@
+//**************previous--one ***************** 
+
+let newDate = new Date()
+
+ //console.log(newDate.toString());
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+const myFunction = function(mks){
+   // console.log("Hello world");
+}
+
+// myFunction();
+
+//***************arrays****************** 
+
+const myarray = ["mayank","singh","rajput",0,1,2,3,4]
+
+console.log(myarray);
+
+console.log(myarray[1]);
+
+//console.log(myarray.push("pagal"));
+myarray.push(6)
+
+console.log(myarray);
+
+console.log(myarray.includes(9));
