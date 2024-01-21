151999c709197a9386cc8f4c5ccff5848c80c067
maynak singh <mayank23378@gmail.com>
2024-01-21 10:33:07 +0000

    adding second array file

---
+++ 01_basics.js/array_02.js
@@ -0,0 +1,20 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+ //marvel_heros.push(dc_heros)
+
+ const myhero = [...marvel_heros,...dc_heros]
+ console.log(myhero);
+
+//  console.log(marvel_heros);
+
+//  console.log(marvel_heros[3]);
+//  console.log(marvel_heros[3][2]);
+ const allHeros = marvel_heros.concat(dc_heros)
+  console.log(allHeros);
+
+  const array = [1,2,3,4,5,[6,7,10,[8,9]]]
+  console.log(array[5][3]);
+
+  const real_array = array.flat(Infinity)
+console.log(real_array);
