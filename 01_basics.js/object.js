a08af7de03884e7c022ca62b8e26ab80fa7f6c6b
maynak singh <mayank23378@gmail.com>
2024-01-21 15:27:21 +0000

    object

--- 01_basics.js/object.js
+++ 01_basics.js/object.js
@@ -15,6 +15,21 @@ console.log(user["subject"][2])
 
 user.name = "mayank kumar "
 console.log(user);
-
+ 
 //***************object-02**************
 
+const usera = { 
+    email : "mayank23378@gmail.com",
+    fullname: { 
+        firstname: "mayank",
+        lastname : "singh"
+    }
+}
+console.log(usera.fullname.firstname);
+  
+const obj = {1:"a", 2 :"b"}
+const obj2 = {3:"c", 4 :"d"}
+const obj3 = {5:"e", 6 :"f"}
+
+const obj4 = {...obj,...obj2,...obj3}
+console.log(obj4);
