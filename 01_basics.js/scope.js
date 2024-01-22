f6947bbaadbf70e6a1f1f7d84e122de4e4629b6b
maynak singh <mayank23378@gmail.com>
2024-01-22 21:40:44 +0000

    adding scope

---
+++ 01_basics.js/scope.js
@@ -0,0 +1,14 @@
+function one(){
+    const username = "mayank"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+     //console.log(website);
+
+     two()
+
+}
+
+ one()   //firstly it execute one and then two(username console inside one) 
