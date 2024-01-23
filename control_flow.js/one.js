6395fee3cbcaee4f3abe8098b5f5ba37f4b79766
maynak singh <mayank23378@gmail.com>
2024-01-23 08:54:48 +0000

    adding first file on control flow

---
+++ control_flow.js/one.js
@@ -0,0 +1,12 @@
+let score = 450 ; 
+
+if (score <= 200) { 
+    console.log(`your score is less then 200`);
+
+} else if (score <= 500){ 
+    console.log(`your score is less then 500`);
+} else if (score <= 800){ 
+    console.log(`your score is less then 900`);
+} else { 
+    console.log(`your score is less then 1000`);
+}
