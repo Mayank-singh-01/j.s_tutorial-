8743410c96d33e1920ce2959ebdca3925dd44167
Mayank singh <mayank23378@gmail.com>
2024-02-05 20:34:58 +0000

    using if loop insted of switch in color change project

--- Practice.js/01_solution.js/practice_01.js
+++ Practice.js/01_solution.js/practice_01.js
@@ -5,25 +5,36 @@ const body = document.querySelector("body");
 mybutton.forEach( function (myfun) {
     myfun.addEventListener( 'click' , function (e) {
         
-          switch (e.target.id) {
-            case 'red':
-                body.style.backgroundColor = e.target.id
-                break;
-            case 'skyblue':
-                body.style.backgroundColor = e.target.id
-                break;
-            case 'pink':
-                body.style.backgroundColor = e.target.id
-                break;
-            case 'gray':
-                body.style.backgroundColor = e.target.id
-                break;
+        //   switch (e.target.id) {
+        //     case 'red':
+        //         body.style.backgroundColor = e.target.id
+        //         break;
+        //     case 'skyblue':
+        //         body.style.backgroundColor = e.target.id
+        //         break;
+        //     case 'pink':
+        //         body.style.backgroundColor = e.target.id
+        //         break;
+        //     case 'gray':
+        //         body.style.backgroundColor = e.target.id
+        //         break;
         
-            default:
-                body.style.backgroundColor = green 
-                break;
+        //     default:
+        //         body.style.backgroundColor = green 
+        //         break;
+        // }
+        if (e.target.id === 'red') {
+            body.style.backgroundColor = e.target.id
         }
-        console.log(e.target);
+        if (e.target.id === 'skyblue') {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'pink') {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'gray') {
+            body.style.backgroundColor = e.target.id
+        }
     })
 } )
 
