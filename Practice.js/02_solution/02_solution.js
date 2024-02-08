c561d629e15419816d5cbd1055125fe27421a9d9
Mayank singh <mayank23378@gmail.com>
2024-02-08 15:52:18 +0530

    injucting j.s in bmi calculator

---
+++ Practice.js/02_solution/02_solution.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form");
+
+form.addEventListener('submit' , function (stopsubmit) {
+    stopsubmit.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#results');
+
+    if (height === '' || height < 0 || isNaN(height)) {
+       result.innerHTML = `Please give a valid Height ${height}`;
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        result.innerHTML = `Please give a valid Weight ${weight}`;
+    } else { 
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+         result.innerHTML = `${bmi}`;
+    }
+});
