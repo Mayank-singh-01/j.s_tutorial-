c1c5310bab3c31d42705d1890682eb43998201a6
Mayank singh <mayank23378@gmail.com>
2024-02-10 03:29:13 +0530

    updating 4th project

--- Practice.js/04_solution/guess.js
+++ Practice.js/04_solution/guess.js
@@ -1,16 +1,16 @@
-let mynum = parseInt((Math.random() * 100 + 1 ));
+let randomNumber = parseInt((Math.random() * 100 + 1 ));
 
+const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
-const submit = document.querySelector('#submit');
-const resultPara = document.querySelector('.resultPara');
 const guessSlot = document.querySelector('.guesses');
+const startOver = document.querySelector('.resultParas');
 const remaining = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
 
 //creating paragraph
 const p = document.createElement('p');
 
-let preveGuess = [];
+let prevGuess = [];
 let numGuess = 1;
 
 let playGame = true;
@@ -31,10 +31,10 @@ function validateGuess(guess) {
     } else if (guess > 100 ) {
         alert('Please enter a number less then 100');
     } else { 
-        preveGuess.push(guess);
+        prevGuess.push(guess);
         if (numGuess === 11 ) {
             displayGuess(guess);
-            displayMassage(`Game over . randim number was ${randomNumber}`);
+            displayMassage(`Game over . random number was ${randomNumber}`);
             endGame();
         } else{ 
           displayGuess(guess);  
@@ -44,14 +44,14 @@ function validateGuess(guess) {
 }
 
 function checkGuess(guess) {
-    if (guess === mynum) {
+    if (guess === randomNumber) {
         // const win = `you win this one`;   // we can use this mathod to 
         // lowOrHi.innerHTML = `${win}`
          displayMassage(`Congrats !! `);
          endGame();
-    } else if (guess < mynum) {
+    } else if (guess < randomNumber) {
         displayMassage(`Your Number is low`);
-    } else if (guess > mynum) {
+    } else if (guess > randomNumber) {
         displayMassage(`your Number is High`);
     }
 }
@@ -64,13 +64,31 @@ function displayGuess(guess) {
 }
 
 function displayMassage(massage) {
-    lowOrHi.innerHTML = `<h2>${massage}</h2>`
+    lowOrHi.innerHTML = `<h2>${massage}</h2>`;
 }
 
-function endGame(params) {
-    
+function endGame() {
+    userInput.value = '';
+    userInput.setAttribute('disabled', '');
+    p.classList.add('button');
+    p.innerHTML = `<h2 class="newone">Start new game </h2>`;
+    startOver.appendChild(p);
+    playGame = false ;
+    newGame();
 }
 
-function startGame(params) {
-    
+function newGame() {
+    const mybutton = document.querySelector('.newone');
+        mybutton.addEventListener('click', function (e) {
+        randomNumber = parseInt(Math.random() * 100 + 1 );
+        prevGuess = [];
+        numGuess = 1;
+        guessSlot.innerHTML = '';
+        remaining.innerHTML = `${11 - numGuess}`;
+        userInput.removeAttribute('disabled');
+        startOver.removeChild(p);
+
+        playGame = true;
+
+    });
 }
