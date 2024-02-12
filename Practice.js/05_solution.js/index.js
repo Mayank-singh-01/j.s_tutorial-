1ad021a0580deaefa5298a309eb6b19b513d401e
Mayank singh <mayank23378@gmail.com>
2024-02-12 22:37:25 +0530

    solving project 06 with different method

--- Practice.js/05_solution.js/index.js
+++ Practice.js/05_solution.js/index.js
@@ -1,11 +1,39 @@
-const rendomColor = function () {
-    const hex = '123456ABCDEF';
+const randomColor = function () {
+    const hex = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
-        color = hex[Math.floor(Math.random() * 16)];
+        color += hex[Math.floor(Math.random()*16)];
+        
     }
     return color;
-}
-const start =  document.querySelector('.button01'); 
+ };
+// let entervalId;
+// const startChangingColor = function(){
+// entervalId = setInterval(changeBgColor,1000);
 
-const end =  document.querySelector('.button02');
+
+// function changeBgColor(){ 
+//     document.body.style.backgroundColor = randomColor();
+// }
+
+// };
+
+// document.querySelector('#button01').addEventListener('click', startChangingColor);
+
+// insted of providing function name directly injecting function .
+
+//i get stuck when it come to provide preference to the stop function , its gonna my first project that i solve from another method myself . 
+
+let set;
+
+document.querySelector('#button01').addEventListener('click',function (){ 
+   function color()  {
+     document.body.style.backgroundColor = randomColor();
+   }
+   set = setInterval(color , 1000)
+});
+
+document.querySelector('#button02').addEventListener('click', function (){ 
+     clearInterval(set);
+})
+
