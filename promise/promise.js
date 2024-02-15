eb6b5c8ccfdc1f11aa44dab60cf16fd458729ab7
Mayank singh <mayank23378@gmail.com>
2024-02-15 23:01:46 +0530

    learning promise

--- promise/promise.js
+++ promise/promise.js
@@ -1,10 +1,21 @@
 const myPromise = new Promise(function(resolve,reject){
     setTimeout(function(){
-        console.log("my promise file work is done");
+        console.log("some async work is called");
         resolve()  // resolve is used to connect this function to then .
     },2000);
 })
 
 myPromise.then(function(){
-    console.log('then is connected');
+    console.log('async work is commpleted');
+})
+
+//***************************another method to write same code******************************
+
+new Promise (function(resolve,reject){
+    setTimeout(function(){
+        console.log('some work is called');
+        resolve()
+    },2000)
+}).then(function(){                      //we can directly jion .then to the promise 
+    console.log('work has been done');
 })
