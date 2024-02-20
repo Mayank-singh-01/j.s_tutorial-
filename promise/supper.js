b5d57a67ef2be15632fc8efcf990f1e5e96f78b1
Mayank singh <mayank23378@gmail.com>
2024-02-20 23:02:43 +0530

    done with supper keyword

--- promise/supper.js
+++ promise/supper.js
@@ -12,14 +12,22 @@ class person {
 
 
 class eng extends person {
-    constructor(branch){
-        super();  // it is used to call parent consteuctor
+    constructor(name , branch){    // we need to pass previous construcor to.
+        super(name);               // it is used to call parent consteuctor
         this.branch = branch ;
     }
     code(){
         console.log('code');
+        super.sleep()              // when we inject sleep it will console the feater of sleep 
+        
     }
 }
 
-const person2 = new eng();
-console.log(person2);
+ const person2 = new eng('akki','bca');
+// console.log(person2.code);
+
+// const person1 = new person('mayank')
+// console.log(person1);
+
+console.log(person2.code());
+
